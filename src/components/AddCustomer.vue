bfa562cb39275ab3188a55e5dab1de36b2d39d9b
karthikragu8695 <karthikragu8695@gmail.com>
2023-12-12 16:52:31 +0530

    b

--- src/components/AddCustomer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <v-container>
-        hiii
-    </v-container>
-</template>
--- src/components/InvoiceTemp.vue
+++ src/components/InvoiceTemp.vue
@@ -3,7 +3,7 @@
       <v-btn class="ml-[70px]" color="primary" @click="dialog=true">New Invoice</v-btn>
 
       <v-text class="d-flex w-[500px]  ml-[70px] mt-5">
-          <v-text-field v-model="userName" label="search">
+          <v-text-field  label="search">
           </v-text-field>
        </v-text>
        <v-table class="p-5">
@@ -20,7 +20,7 @@
                 <td>1</td>
                 <td>karthik</td>
                 <td>20000</td>
-                <td>1</td>
+                <td></td>
               </tr>
             </thead>
         </v-table>
@@ -29,98 +29,97 @@
         
           <v-table>
               <v-text class="d-flex w-[1000px]  mt-10">
-                <v-text-field 
+                <v-autocomplete 
                       class="ml-10"
-                      :items="items"
                       placeholder="Name"
                       label="Customer name"
+                      item-title="name"
+                      :items="customers"
+                      v-model="customer"
+                      item-value="iid"
                       variant="outlined">
-                </v-text-field>
-                <v-text-field 
+                </v-autocomplete>
+                <v-autocomplete
                       class="ml-5 "
-                      :items="items"
-                      v-model="id"
-                      placeholder="Reg Num"
-                      label="Register Number"
+                      placeholder="phone number"
+                      label="Phone Number"
+                      item-title="phone"
+                      :items="customers"
+                      item-value="iid"
+                      v-model="customer"
                       variant="outlined">
-                </v-text-field>
-                <v-text-field
+                </v-autocomplete>
+                <v-autocomplete
                       class="ml-5"
-                      v-model="tax"
-                      :items="items"
                       placeholder="2021-12-09"
                       label="Invoice Date"
                       variant="outlined">
-                </v-text-field>
+                </v-autocomplete>
               </v-text>
-              <v-btn color="primary" class="ml-10" @click="customer=true" >Add Customer</v-btn>
-              <v-btn color="primary" class="ml-5" >Add Product</v-btn>
+              <v-text class="d-flex w-[1000px]  mt-10">
+                <v-autocomplete 
+                      class="ml-10"
+                      placeholder="Id"
+                      label="product Id"
+                      item-title="id"
+                      :items="products"
+                      v-model="product"
+                      item-value="iid"
+                      variant="outlined">
+                </v-autocomplete>
+                <v-autocomplete
+                      class="ml-5 "
+                      placeholder="product Name"
+                      label="product Name"
+                      item-title="name"
+                      :items="products"
+                      item-value="iid"
+                      v-model="product"
+                      variant="outlined">
+                </v-autocomplete>
+                <v-autocomplete
+                      class="ml-5"
+                      placeholder="product price"
+                      :items="products"
+                      item-value="iid"
+                      item-title="price"
+                      v-model="product"
+                      label="Date"
+                      variant="outlined">
+                </v-autocomplete>
+              </v-text>
             </v-table>
+
             <v-table class="px-15">
               <thead>
               <tr>
-                <th>Service</th>
-                <th>Description</th>
-                <th>Quantity</th>
-                <th>Rate</th>
+                <th>product Name</th>
+                <th>product Id</th>
+                <th>product price</th>
                 <th>Amount</th>
-                <th>Action</th>
+                <th>Actions </th>
               </tr>
               <tr>
-                <td>Dental</td>
-                <td>Full Month</td>
+                <td>oil</td>
                 <td>1</td>
                 <td>1200</td>
                 <td>1220</td>
-                <td>delete</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                  <v-text-field placeholder="0" label="Total Amount" readonly variant="outlined"> 
+                </v-text-field>
+                </td>
               </tr>
             </thead>
+            
             </v-table>
-          </v-dialog>
-          <!---------------------------------Inside customer--------------------------------->
-          <v-dialog v-model="customer">
-            <v-card  class="mx-auto"
-                        max-width="344">
-              <v-card-title>Customer Details</v-card-title>
-              <v-container >
-                    <v-text-field
-                        v-model="id"
-                        color="primary"
-                        label="Customer Id"
-                        variant="underlined">
-                    </v-text-field>
-
-                    <v-text-field
-                        v-model="name"
-                        color="primary"
-                        label="Customer name"
-                        variant="underlined">
-                    </v-text-field>
-
-                    <v-text-field
-                        v-model="phone"
-                        color="primary"
-                        label="Customer Phone"
-                        variant="underlined">
-                    </v-text-field>
-
-                    <v-checkbox
-                        v-model="terms"
-                        color="secondary"
-                        label="I agree to site terms and conditions">
-                    </v-checkbox>
-                </v-container>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-                 <v-btn color="red" @click="close">close</v-btn>
-                  <v-btn  else color="success">Save
-                    <v-icon icon="mdi-chevron-right" end></v-icon>
-                  </v-btn>
-                  <v-btn color="success">update
-                      <v-icon icon="mdi-chevron-right" end></v-icon>
-                  </v-btn>
-            </v-card-actions>
-            </v-card>
+            
           </v-dialog>
     </v-container>
 </template>
@@ -130,8 +129,26 @@ export default{
   data(){
     return{
       dialog:false,
-      customer:false,
+      show:false,
+      showitem:false,
+      editing:false,
+      customer:null,
+      product:null
     }
-  }, 
-}
+  },
+  methods:{
+    close(){
+      this.showitem=false
+      this.show=false
+    }, 
+    },
+    computed:{
+      customers(){
+        return this.$store.getters.customerDetails
+      },
+      products(){
+        return this.$store.getters.productDetails
+      }
+    }
+  }
 </script>
